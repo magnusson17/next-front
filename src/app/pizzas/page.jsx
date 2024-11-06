70ccc3d27563950dee5404b4d03685ea4d5cd65f
Gabriele Magni <gabri.magni.97@gmail.com>
2024-11-06 14:43:07 +0100

    test 7 dynamic

--- src/app/pizzas/page.jsx
+++ src/app/pizzas/page.jsx
@@ -10,7 +10,9 @@ export default async function Pizzas() {
     const singlePage = await getSingleContent(process.env.GET_ALL_PAGES, "nid", 4)
     const blocchiIds = await arrayIdBlocchi(singlePage)
     const pizzas = await fetchRequest(process.env.GET_ALL_PIZZAS)
-
+    
+    console.log(pizzas)
+    
     return (
         <main className="relative page-pizze">
             <h1>{singlePage.field_titolo_main}</h1>
