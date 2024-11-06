e9382c8209f737139603f5bc4743e470a5e303a0
Gabriele Magni <gabri.magni.97@gmail.com>
2024-11-06 11:41:10 +0100

    test 5 dynamic

--- src/app/pizzas/page.jsx
+++ src/app/pizzas/page.jsx
@@ -1,16 +1,17 @@
-export const dynamic = "force-dynamic";
-
 import { fetchRequest, arrayIdBlocchi, getSingleContent } from "@/components/functions/requestFunctions";
 import AllBlocchi from "@/components/blocchi/AllBlocchi";
 import clearString from "@/components/functions/clearString";
 import Link from "next/link";
 
-export default async function Pizzas() {
+export async function getServerSideProps() {
+    const singlePage = await getSingleContent(process.env.GET_ALL_PAGES, "nid", 4);
+    const blocchiIds = await arrayIdBlocchi(singlePage);
+    const pizzas = await fetchRequest(process.env.GET_ALL_PIZZAS);
 
-    const singlePage = await getSingleContent(process.env.GET_ALL_PAGES, "nid", 4)
-    const blocchiIds = await arrayIdBlocchi(singlePage)
-    const pizzas = await fetchRequest(process.env.GET_ALL_PIZZAS)
+    return { props: { singlePage, blocchiIds, pizzas } };
+}
 
+export default function Pizzas({ singlePage, blocchiIds, pizzas }) {
     return (
         <main className="relative page-pizze">
             <h1>{singlePage.field_titolo_main}</h1>
