dcf0acdf47542723a1d19164753c8b130745850b
Gabriele Magni <gabri.magni.97@gmail.com>
2024-11-12 10:16:39 +0100

    aggiunta commenti in webhooks

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,15 +1,14 @@
 import { NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
-
+// il webhook invia dati a questo endpoint con metodo POST
+// uso la fun POST per prendere la req del webhook
 export async function POST(request) {
-    // Logic to handle incoming webhook request
+    // gestisco e loggo la req (facoltativo)
     const body = await request.json();
-    
-    // Process the webhook data as needed
     console.log("Received webhook data:", body);
-    
+    // rivalido il path specifico
     revalidatePath('/pizzas');
-
+    // ritorno una risposta a Drupal per confermare la ricezione del webhooks
     return NextResponse.json({ message: "Webhook received successfully" });
 }
 
